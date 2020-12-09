d2537308a79ecb5d1c4ce8b67ccc54a799cef1ee
AshishSah10 <41590024+AshishSah10@users.noreply.github.com>
2020-12-09 15:10:49 +0530

    change1

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,12 @@
 // import using require
+let Shape = require('./shape.js');
 
 // declare class
+class Circle extends Shape{
+  calculateArea(r){
+    return (22/7)*r*r;
+  }
+}
 
-
+module.exports = Circle();
 // export class using module.exports
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,10 @@
 // declare class
-
+class Shape {
+  constructor(color) {
+    this.color = color;
+  }
+  drawShape() {}
+  calculateArea() {}
+}
+module.exports = Shape();
 // export class using module.exports
